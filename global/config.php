73f3ac3366affd3feb3562ccd0d7667a6de845fa
HEITZ Martin <martin.heitz@uds-501607.ad.unistra.fr>
2017-12-14 14:27:33 +0100

    modifications

--- global/config.php
+++ global/config.php
@@ -1,9 +1,9 @@
 <?php
 
 // Identifiants pour la base de données. Nécessaires a PDO2.
-define('SQL_DSN',      'mysql:dbname=mvc;host=localhost');
-define('SQL_USERNAME', 'root');
-define('SQL_PASSWORD', '');
+define('SQL_DSN',      'mysql:host=osr-mysql.unistra.fr;dbname=martin_heitz');
+define('SQL_USERNAME', 'martin_heitz');
+define('SQL_PASSWORD', 'bddlove68');
 
 // Chemins à utiliser pour accéder aux vues/modeles/librairies
 $module = empty($module) ? !empty($_GET['module']) ? $_GET['module'] : 'index' : $module;
