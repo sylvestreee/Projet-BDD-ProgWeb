0b3d3d4077f2ff122906335bb87fdb3e37673dc5
morganeritter@hotmail.fr <morganeritter@uds-501609.ad.unistra.fr>
2017-12-15 10:45:13 +0100

    recherche recette

---
+++ modules/recette/afficher_recette.php
@@ -0,0 +1,37 @@
+<?php
+
+session_start();
+
+// Pas de vérification de droits d'accès nécessaire
+
+include CHEMIN_LIB.'form.php';
+
+// "formulaire_recherche_nom" est l'ID unique du formulaire
+$form_recherche_nom = new Form('formulaire_recherche_nom');
+
+$form_recherche_nom->method('POST');
+
+$form_recherche_nom->add('Text', 'phrase')
+			   ->label("Rechercher une recette");
+
+// Création d'un tableau des erreurs
+$erreurs_connexion = array();
+
+// Validation des champs suivant les règles
+if ($form_connexion->is_valid($_POST)) {
+	
+	list($phrase) = $form_recherche_nom('phrase');
+	
+	include CHEMIN_MODELE.'recette.php';
+	
+	$id_recette = recherche_recette_par_nom($phrase);
+	
+	while($row=mysql_fetch_array($id_recette))
+	{
+		echo $row;
+	}	
+}
+else
+{
+	include CHEMIN_VUE.'afficher_resultat_recherche_recette.php';		
+}
---
+++ modules/recette/vues/afficher_resultat_recherche_recette.php
@@ -0,0 +1,3 @@
+<h2> Resultat de la recherche pour des recettes dont le nom contient <?php echo htmlspecialchars($phrase)?> </h2>
+
+<?php echo $form_recherche_nom; ?>
