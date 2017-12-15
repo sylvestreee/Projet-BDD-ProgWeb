c1a656e70853e1f96687911c098c8cc5ef3605ec
HEITZ Martin <martin.heitz@uds-501608.ad.unistra.fr>
2017-12-15 08:10:40 +0100

    changement nom

--- modeles/inscription.php
+++ modeles/inscription.php
@@ -5,11 +5,11 @@ function ajouter_membre_dans_bdd($nom_utilisateur, $prenom, $nom, $mdp, $adresse
 	$pdo = PDO2::getInstance();
 
 	$requete = $pdo->prepare("INSERT INTO UTILISATEUR SET
-		login 	= :nom_utilisateur,
-		prenom 	= :prenom,
-		nom 	= :nom,
-		mdp 	= :mot_de_passe,
-		email 	= :adresse_email");
+		login 				= :nom_utilisateur,
+		prenom 				= :prenom,
+		nom_utilisateur 	= :nom,
+		mdp 				= :mot_de_passe,
+		email 				= :adresse_email");
 
 	$requete->bindValue(':nom_utilisateur', $nom_utilisateur);
 	$requete->bindValue(':prenom',   		$prenom);
