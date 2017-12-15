d1c4bf02f95a82696f4884b7ff8c0e9642dd9ff8
HEITZ Martin <martin.heitz@uds-501608.ad.unistra.fr>
2017-12-15 10:40:07 +0100

    maj creation recette

--- modules/membres/creation_recette.php
+++ modules/membres/creation_recette.php
@@ -54,13 +54,12 @@ else
 	// Validation des champs suivant les règles en utilisant les données du tableau $_POST
 	if ($crea_recette->is_valid($_POST)) 
 	{
-
 		// On vérifie si nb_personnes est bien un nombre (is_numeric)
-		/*if (ctype_digit($crea_recette->get_cleaned_data('nb_personnes')))
+		if (ctype_digit($crea_recette->get_cleaned_data('nb_personnes')))
 		{
 			$erreurs_recette[] = "Vous n'avez pas entré un nombre de personnes valide";
-			echo $crea_recette->get_cleaned_data('nb_personnes');
-		}*/
+			//echo $crea_recette->get_cleaned_data('nb_personnes');
+		}
 
 		// Si d'autres erreurs ne sont pas survenues
 		if (empty($erreurs_recette)) 
