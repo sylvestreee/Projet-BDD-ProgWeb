f5b51dfe2fdd443fecfb5a04df8d36c2d04863cb
morganeritter@hotmail.fr <morganeritter@uds-501609.ad.unistra.fr>
2017-12-15 09:01:44 +0100

    test

--- modules/membres/vues/profil_infos_utilisateur.php
+++ modules/membres/vues/profil_infos_utilisateur.php
@@ -1,6 +1,6 @@
 <h2>Profil de <?php echo htmlspecialchars($_SESSION['pseudo']); ?></h2>
 
 <p>
-    <span class="label_profil">Nom d'utilisateur</span> : <?php echo htmlspecialchars($nom_utilisateur); ?><br/>
+    <span class="label_profil">Nom d'utilisateur</span> : <?php echo htmlspecialchars($login); ?><br/>
     <span class="label_profil">Adresse email</span> : <?php echo htmlspecialchars($email); ?><br/>
  </p>
