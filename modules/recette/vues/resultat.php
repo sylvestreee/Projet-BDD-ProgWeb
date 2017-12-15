3b01e45e0632dd715875f98cb0a600e12f10b97f
morganeritter@hotmail.fr <morganeritter@uds-501609.ad.unistra.fr>
2017-12-15 12:24:41 +0100

    recherche recette

--- modules/recette/vues/resultat.php
+++ modules/recette/vues/resultat.php
@@ -1,8 +1,9 @@
 <h2> Resultat de la recherche pour des recettes dont le nom contient <?php echo htmlspecialchars($phrase)?> </h2>
 
 <?php
+var_dump($id_recette);
 foreach($id_recette as $r)
 {
-	echo $r;
+	echo $r["nom_recette"];
 	var_dump($r);
 }
