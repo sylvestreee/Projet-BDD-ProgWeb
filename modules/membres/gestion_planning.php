8f05bbd4275d15609b5e6c60bcef913e30c94bb6
sylvestreee <martin-heitz@orange.fr>
2017-12-18 17:53:53 +0100

    test

--- modules/membres/gestion_planning.php
+++ modules/membres/gestion_planning.php
@@ -45,8 +45,8 @@ else
 					  ->label("Date du repas (dd/mm/yy)");
 
 	$gest_planning    ->add('Date', 'heure')
-					  ->format('HH:MM')
-					  ->label("Heure du repas (HH:MM)");
+					  ->format('HH/MM')
+					  ->label("Heure du repas (HH/MM)");
 
 	$gest_planning 	  ->add('Submit', 'recette')
 					  ->value("Ajouter la recette");
