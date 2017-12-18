bda6965b6eaeb58a8eac9a364adafdfa042558b5
sylvestreee <martin-heitz@orange.fr>
2017-12-18 10:59:18 +0100

    test

--- modules/recette/vues/affichage_info_recette.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-echo $info_recette["nom_recette"]."<br>"; 
-echo $info_recette["descriptif"]."<br>"; 
-echo $info_recette["difficulte"]."<br>"; 
-echo $info_recette["prix"]."<br>";
-echo $info_recette["nb_personnes"]."<br>"; 
---
+++ modules/recette/vues/affichage_recette.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "Nom de la recette : ".$info_recette["nom_recette"]."<br>"; 
+echo "Auteur : ".$info_recette["auteur"]."<br>"; 
+echo "Descriptif : ".$info_recette["descriptif"]."<br>"; 
+echo "Difficulte : " $info_recette["difficulte"]."<br>"; 
+echo "Prix : ".$info_recette["prix"]."<br>";
+echo "Nombre de personnes : ".$info_recette["nb_personnes"]."<br>"; 
