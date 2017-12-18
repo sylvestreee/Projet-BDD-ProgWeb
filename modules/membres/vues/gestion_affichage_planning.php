b9673b5784b2bd10315321904e7cee91432be2be
sylvestreee <martin-heitz@orange.fr>
2017-12-18 17:02:43 +0100

    test

--- modules/membres/vues/gestion_affichage_planning.php
+++ modules/membres/vues/gestion_affichage_planning.php
@@ -6,3 +6,8 @@
 
 echo $recette;
 echo $gest_planning;
+
+$monday = date('dd/mm', strtotime('monday this week'));
+$sunday = date('dd/mm', strtotime('sunday this week'));
+
+echo "Planning de la semaine du".$monday." au ".$sunday;
