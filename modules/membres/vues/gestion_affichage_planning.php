7f4aef75064134c76a974d23ba200b2ee941f477
sylvestreee <martin-heitz@orange.fr>
2017-12-18 17:12:42 +0100

    test

--- modules/membres/vues/gestion_affichage_planning.php
+++ modules/membres/vues/gestion_affichage_planning.php
@@ -7,7 +7,24 @@
 echo $recette;
 echo $gest_planning;
 
+// PLANNING
+
 $monday = date('d/m', strtotime('monday this week'));
+$tuesday = date('d/m', strtotime('tuesday this week'));
+$wednesday = date('d/m', strtotime('wednesday this week'));
+$thursday = date('d/m', strtotime('thursday this week'));
+$friday = date('d/m', strtotime('friday this week'));
+$saturday = date('d/m', strtotime('saturday this week'));
 $sunday = date('d/m', strtotime('sunday this week'));
 
-echo "Planning de la semaine du ".$monday." au ".$sunday;
+echo "Planning de la semaine du ".$monday." au ".$sunday."<br>"."<br>";
+
+echo "Lundi ".$monday."<br>"."<br>";
+echo "Mardi ".$tuesday."<br>"."<br>";
+echo "Mercredi ".$wednesday."<br>"."<br>";
+echo "Mardi ".$thursday."<br>"."<br>";
+echo "Lundi ".$friday."<br>"."<br>";
+echo "Lundi ".$saturday."<br>"."<br>";
+echo "Lundi ".$sunday."<br>"."<br>";
+
+
